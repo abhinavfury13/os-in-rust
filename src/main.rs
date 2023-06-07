b869fc8908c40be2c5a119b7f9bad3d4947e2087
abhinavfury13 <abhinavramnath13@gmail.com>
2023-06-07 19:59:32 +0530

    Printing to VGA Text Buffer through qemu

--- src/main.rs
+++ src/main.rs
@@ -11,5 +11,16 @@ fn panic(_info: &PanicInfo) -> !{ //! is an operator that means the function is 
 
 #[no_mangle] //We use no_mangle so that the compiler doesn't change the name of the function that we define
 pub extern "C" fn _start() -> !{ //We need this to tell the linker the entry point
+    let buffer: *mut u8 = 0xb8000 as *mut u8; //we map the VGA Buffer address as a mutable pointer
+    let text = b"My Own VGA Text Buffer!";
+    let mut i=0;
+    for &char in text{
+        unsafe{
+            *buffer.offset(i*2) = char; //The character to be printed
+            *buffer.offset(i*2+1) = 0x2; //8-bit color
+            i+=1;
+        }
+    }
+        
     loop{}
 }
